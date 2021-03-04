bd0a6fcaa2b1faccc40558a7ecef7a444550340d
German <bz5h7@yandex.ru>
2021-03-04 15:28:43 +0300

    feat: add 'Extended repeater' solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' }) {
+  const stringToRepeat = `${str}`;
+
+  const tokens = [];
+  const additionalTokens = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    tokens.push(`${stringToRepeat}`);
+  }
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    additionalTokens.push(`${addition}`);
+  }
+  let additionalString = additionalTokens.join(`${additionSeparator}`);
+  
+  let resultString = tokens.map(token => `${token}${additionalString}`).join(`${separator}`);
+  
+  return resultString;
 };
   
