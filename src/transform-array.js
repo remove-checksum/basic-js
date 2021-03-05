eb480745305891a9274c56480cde3960eefa1ecb
German <bz5h7@yandex.ru>
2021-03-05 19:10:50 +0300

    feat: implement 'transform' function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('argument provided not an array');
+
+  let result = [];
+
+  for (let idx = 0; idx < arr.length; idx++) {
+    switch (arr[idx]) {
+      case '--discard-next':
+        if (idx < (arr.length - 1)) idx++;
+        break;
+
+      case '--discard-prev':
+        if (idx > 0 && (arr[idx - 2] !== '--discard-next')) result.pop(); // ??? > [--discard-prev] AND [ not(--discard-next) > VAL > --discard-prev ]
+        break;
+
+      case '--double-next':
+        if (idx < (arr.length - 1)) result.push(arr[idx + 1]); // [--double-next] > ???
+        break;
+
+      case '--double-prev':
+        if (idx > 0 && (arr[idx - 2] !== '--discard-next')) result.push(arr[idx - 1]); // ??? > [--double-prev] AND [ not(--discard-next) > VAL > --double-prev ]
+        break;
+        
+      default:
+        result.push(arr[idx]);
+        break;
+    }
+  }
+  return result;
 };
